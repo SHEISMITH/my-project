8ccbe5f6e688a89369e6a9faa18d3ef0b9b93766
sheismith <sheismith99@gmail.com>
2018-08-03 10:16:09 +0100

    my first project

---
+++ sumarray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int sumarray(a[],size);
+int main();
+{
+int a[4]={1,2,3,4};
+int size =sizeof(array,size);
+int result =sumarray(array,size);
+printf("%d\n",result);
+return 0
+}
+int sumarray(a[],size);{
+int size,i,a[];
+int sum=0;
+for(i=0;i<size;i++);
+{
+sum=sum+a[i];
+}
+return sum
+}
+
+
